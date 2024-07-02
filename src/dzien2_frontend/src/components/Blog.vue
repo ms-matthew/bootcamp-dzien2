0e791d810e9a26902a93c6c7399c089df1772751
ms-matthew <mateusz.stachowicz1@wp.pl>
2024-07-02 04:19:57 +0200

    Added editing wpisy and fixed text field

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -1,53 +1,58 @@
 <template>
     <div>
-        <h2 class="text-blue-600">Wpisy na bloga:</h2>
+        <h1 class="text-blue-600">Wpisy na bloga</h1>
         <div class="w-100 flex flex-row-reverse">
             <button @click="pobierzWpisy" class="bg-blue-600 rounded text-white p-4">refresh</button>
         </div>
         <div class="grid mx-6 gap-4 my-4">
-            <div v-for="(wpis, index) in wpisy" class="drop-shadow-xl bg-stone-300 p-4">
-                <p>id: {{ index }}</p>
-                <p>{{ wpis }}</p>
-                <button  class="bg-blue-600 rounded text-white p-4" @click="deleteWpis(index)">usun</button>
-                <input v-model="editedText" class="border-2 border-blue-600 p-4" type="text">
-                <button  class="bg-blue-600 rounded text-white p-4" @click="edytujWpisy(index, editedText)">edytuj</button>
+        <div v-for="(wpis, index) in wpisy" class="drop-shadow-x1 bg-stone-300 p-4">
+            <p>{{ wpis }}</p>
+            <button @click="usunWpis(index)" class="bg-blue-600 rounded text-white p-4">Usun</button>
+            <div>
+                <input type="text" v-model="edycja[index]" class="border-2 border-blue-600 p-4">
+                <button @click="edytujWpis(index, edycja[index])" class="bg-blue-600 rounded text-white p-4">Edytuj</button>
             </div>
+
+        </div>
         </div>
         <div class="flex justify-center flex-col">
-            <input v-model="nowyBlog" class="border-2 border-blue-600 p-4" type="text">
-            <button  class="bg-blue-600 rounded text-white p-4" @click="dodajWpisy">dodaj</button>
+            <input v-model="nowyBlog" type="text" class="border-2 border-blue-600 p-4">
+            <button @click="dodajWpis" class="bg-blue-600 rounded text-white p-4">Dodaj</button>
         </div>
     </div>
 </template>
 
 <script>
 import { dzien2_backend } from 'declarations/dzien2_backend/index';
-
-export default {
-    data() {
-        return {
-            wpisy: [],
-            nowyBlog: ""
-        }
-    },
-    methods: {
-        async dodajWpisy() {
-            await dzien2_backend.dodaj_wpis(this.nowyBlog);
-            await this.pobierzWpisy();
+    export default{
+        data(){
+            return{
+                wpisy: [],
+                nowyBlog: "",
+                edycja: []
+            }
         },
-        async deleteWpis(index) {
-            await dzien2_backend.usun_wpis(index);
-            await this.pobierzWpisy();
-        },
-        async pobierzWpisy() {
-            this.wpisy = await dzien2_backend.odczytaj_wpisy();
-        },
-        async edytujWpisy(){
-            await dzien2_backend.edytuj_wpis(index, this.editedText);
-        }
-    },
-    async mounted(){
-        this.pobierzWpisy()
-    }
-}
+        methods: {
+            async dodajWpis(){
+                await dzien2_backend.dodaj_wpis(this.nowyBlog);
+                await this.pobierzWpisy();
+                this.nowyBlog = null;
+            },
+            async usunWpis(index){
+                await dzien2_backend.usun_wpis(index);
+                await this.pobierzWpisy();
+            },
+            async pobierzWpisy(){
+                this.wpisy = await dzien2_backend.odczytaj_wpisy();
+            },
+            async edytujWpis(index, nowyWpis){
+                await dzien2_backend.edytuj_wpis(index, nowyWpis);
+                await this.pobierzWpisy();
+                this.edycja[index] = null;
+            },
+            async mounted(){
+                this.pobierzWpisy();
+            }
+            
+    }}
 </script>
